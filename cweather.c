c55776f6023de3864950b7af95af75ebba351147
JRAMBO720 <55607173+JRAMBO720@users.noreply.github.com>
2021-12-01 14:56:39 -0500

    Update cweather.c

--- cweather.c
+++ cweather.c
@@ -1,19 +1,44 @@
-// 
+// Last Revision: 12/1/2021 by Jon 
 
 #include <stdlib.h>
 #include <stdio.h>
 
 // ----------------------------------------------------------------------------
 // Prototypes
-
+void displayMenu();
 
 // ----------------------------------------------------------------------------
 
 int main()
 {
-
+    displayMenu();
     return 0;
 }
 
 // ----------------------------------------------------------------------------
 // Function declarations
+
+// weather conditions, humidity, temperature, wind, location, precipitation, moon phases, etc
+void displayMenu()
+{
+  printf("\nWhat would you like to view for **INCLUDE LOCATION SOMEHOW***\n");
+  printf("Select the number corresponding to the function you'd like to view.\n");
+  printf("For example, 1 would display the Weather Condition, 2 would display Humidity, etc.\n");
+  printf("\n+------------------------+\n");
+  printf("| 1 | Weather Condition  |\n");
+  printf("|------------------------|\n");
+  printf("| 2 | Humidity           |\n");
+  printf("|------------------------|\n");
+  printf("| 3 | Temperature        |\n");
+  printf("|------------------------|\n");
+  printf("| 4 | Wind               |\n");
+  printf("|------------------------|\n");
+  printf("| 5 | Location           |\n");
+  printf("|------------------------|\n");
+  printf("| 6 | Precipitation      |\n");
+  printf("|------------------------|\n");
+  printf("| 7 | Moon Phases        |\n");
+  printf("|------------------------|\n");
+  printf("| 8 | History            |\n");
+  printf("+------------------------+\n");
+}
