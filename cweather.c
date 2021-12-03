94411cf7b90d1d293bb2a7e3c2e4721b7ca3afeb
JRAMBO720 <55607173+JRAMBO720@users.noreply.github.com>
2021-12-02 22:54:00 -0500

    Update cweather.c

--- cweather.c
+++ cweather.c
@@ -5,36 +5,38 @@
 // If you are getting an error about #include <curl/curl.h>
 // you need to run: sudo apt install libcurl4-openssl-dev
 
+// sudo apt install libjson-c-dev
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <curl/curl.h>
+#include <json-c/json.h>
+#include <unistd.h>
+#include <string.h>
 
 // ----------------------------------------------------------------------------
 // Prototypes
 void displayMenu();
+void getWeather();
 
 // ----------------------------------------------------------------------------
 
+static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+  return written;
+}
+
 int main()
 {
-    displayMenu();
+  getWeather();
+  displayMenu();
+  
+ 
+  return 0;
 
-
-    // Testing out curl function
-    CURL *curl;
-    CURLcode result;
-    curl = curl_easy_init();
-
-    // Set URL
-    curl_easy_setopt(curl, CURLOPT_URL, "https://wttr.in/?format=j1"); // This will return the results in JSON format.
-
-    // Perform the request which prints to stdout.
-    result = curl_easy_perform(curl);
-
-
-    curl_easy_cleanup(curl);
-    return 0;
 }
+
 
 // ----------------------------------------------------------------------------
 // Function declarations
@@ -65,3 +67,45 @@ void displayMenu()
   printf("+------------------------+\n");
 }
 
+void getWeather()
+{
+  CURL *curl_handle;
+  static const char *pagefilename = "page.out";
+  FILE *pagefile;
+ 
+  curl_global_init(CURL_GLOBAL_ALL);
+ 
+  /* init the curl session */
+  curl_handle = curl_easy_init();
+ 
+  /* set URL to get here */
+  curl_easy_setopt(curl_handle, CURLOPT_URL, "https://wttr.in/?format=j1");
+ 
+  /* Switch on full protocol/debug output while testing */
+  curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+ 
+  /* disable progress meter, set to 0L to enable it */
+  curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+ 
+  /* send all data to this function  */
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+ 
+  /* open the file */
+  pagefile = fopen(pagefilename, "wb");
+  if(pagefile) {
+ 
+    /* write the page body to this file handle */
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
+ 
+    /* get it! */
+    curl_easy_perform(curl_handle);
+ 
+    /* close the header file */
+    fclose(pagefile);
+  }
+ 
+  /* cleanup curl stuff */
+  curl_easy_cleanup(curl_handle);
+ 
+  curl_global_cleanup();
+}
