c3133c4e7ef2f82cfc660c9abfcf944164f38fdd
JRAMBO720 <55607173+JRAMBO720@users.noreply.github.com>
2021-12-04 11:34:39 -0500

    Update json-practice.c

--- json-practice.c
+++ json-practice.c
@@ -1,23 +1,26 @@
-// Last edited by: Jon 12/02/2021
-// Temperature, humidity, precipitation, and wind work
+// Last edited by: Jon 12/04/2021
+// Temperature, humidity, precipitation, wind, and location work
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <curl/curl.h>
 #include <json-c/json.h>
 
+// Command to compile: gcc -o json json.c -ljson-c
 
 void getHumidity();
 void getPrecipitation();
 void getWind();
 void getTemp();
+void getLocation();
 
 int main() 
 {
-	getTemp();
-	getHumidity();
-	getPrecipitation();
-	getWind();
+	// getTemp();
+	// getHumidity();
+	// getPrecipitation();
+	// getWind();
+	getLocation();
 	return 0;
 }
 
@@ -97,7 +100,7 @@ void getWind()
 	}
 }
 
-void getTemp()
+void getTemp()  // Add feels like
 {
 	int arraylen, i;
 	struct json_object *current_condition_obj, *current_condition_array, *current_condition_array_obj, *temp_F, *temp_C;
@@ -119,5 +122,47 @@ void getTemp()
 		printf("temp_F=%s\n", json_object_get_string(temp_F));
 		printf("temp_C=%s\n", json_object_get_string(temp_C));
 	}
+
+}
+
+void getLocation()  
+{
+	int arraylen, i;
+	struct json_object *nearest_area_obj, *nearest_area_array, *nearest_area_array_obj, *areaName, *region;
+	struct json_object *areaName_obj, *areaNameVal, *region_obj, *regionVal;
+	static const char filename[] = "test.json";
+	nearest_area_obj = json_object_from_file(filename);
+	nearest_area_array = json_object_object_get(nearest_area_obj, "nearest_area");
+
+	// nearest_area_array is an array of objects
+	arraylen = json_object_array_length(nearest_area_array);
+
+	for (i = 0; i < arraylen; i++) {
+		// get the i-th object in nearest_area_array
+		nearest_area_array_obj = json_object_array_get_idx(nearest_area_array, i);
+
+		// get the areaName and region attributes in the i-th object
+		areaName = json_object_object_get(nearest_area_array_obj, "areaName");
+		region = json_object_object_get(nearest_area_array_obj, "region");
+
+		// print out the areaName and region attributes.
+		// printf("areaName=%s\n", json_object_get_string(areaName));
+		// printf("region=%s\n", json_object_get_string(region));
+	}
+	
+	// Gets the value of area Name
+
+		areaName_obj = json_object_array_get_idx(areaName,0);
+
+		areaNameVal = json_object_object_get(areaName_obj, "value");
+
+		printf("value=%s\n", json_object_get_string(areaNameVal));
+
+	// Gets the value of region
+		region_obj = json_object_array_get_idx(region,0);
+
+		regionVal = json_object_object_get(region_obj, "value");
+
+		printf("value=%s\n", json_object_get_string(regionVal));
 
 }
