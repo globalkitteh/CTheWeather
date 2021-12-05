87e0595cd58afdd225782e3984c97527dd55db52
globalkitteh <peterjust.d@gmail.com>
2021-12-05 01:22:08 -0500

    indentation

--- cweather.c
+++ cweather.c
@@ -33,81 +33,79 @@ void partlyCloudy();
 
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 {
-  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
-  return written;
+	size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+	return written;
 }
 
 int main()
 {
-  // Gathers the weather information and stores it in JSON format to page.json
-  getWeather();
+	// Gathers the weather information and stores it in JSON format to page.json
+	getWeather();
 
-  int repeat = 1;
-  while(repeat == 1)
-  {
-  // Displays the interactive menu.
-  displayMenu();
+	int repeat = 1;
+	while (repeat == 1)
+	{
+		// Displays the interactive menu.
+		displayMenu();
 
-  int decision = 0;
-  scanf("%d", &decision);
-  switch (decision)
-  {
-  case 1:
-    break;
-  
-  case 2:
-    getLocation();
-    getHumidity();
-    exit(1);
-    break;
+		int decision = 0;
+		scanf("%d", &decision);
+		switch (decision)
+		{
+		case 1:
+			break;
 
-  case 3:
-    getLocation();
-    getTemp();
-    exit(1);
-    break;
+		case 2:
+			getLocation();
+			getHumidity();
+			exit(1);
+			break;
 
-  case 4:
-    getLocation();
-    getWind();
-    exit(1);
-    break;
+		case 3:
+			getLocation();
+			getTemp();
+			exit(1);
+			break;
 
-  case 5:
-    getLocation();
-    exit(1);
-    break;
+		case 4:
+			getLocation();
+			getWind();
+			exit(1);
+			break;
 
-  case 6:
-    getLocation();
-    getPrecipitation();
-    exit(1);
-    break;
+		case 5:
+			getLocation();
+			exit(1);
+			break;
 
-  case 7:
-    getLocation();
-    getAstronomy();
-    exit(1);
-    break;
+		case 6:
+			getLocation();
+			getPrecipitation();
+			exit(1);
+			break;
 
-  case 8:
-    // Testing design functions
-    sun();
-    cloud();
-    rain();
-    partlyCloudy();
-    exit(1);
-    break;
+		case 7:
+			getLocation();
+			getAstronomy();
+			exit(1);
+			break;
 
-  default:
-    break;
-  }
-  }
+		case 8:
+			// Testing design functions
+			sun();
+			cloud();
+			rain();
+			partlyCloudy();
+			exit(1);
+			break;
 
-  return 0;
+		default:
+			break;
+		}
+	}
 
+	return 0;
 }
-
 
 // ----------------------------------------------------------------------------
 // Function declarations
@@ -116,32 +114,32 @@ int main()
 // on what they want to view.
 void displayMenu()
 {
-  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-  printf("\nWhat would you like to view for **INCLUDE LOCATION SOMEHOW***\n");
-  printf("Select the number corresponding to the function you'd like to view.\n");
-  printf("For example, 1 would display the Weather Condition, 2 would display Humidity, etc.\n");
-  printf("\n+------------------------+\n");
-  printf("| 1 | Weather Condition  |\n");
-  printf("|------------------------|\n");
-  printf("| 2 | Humidity           |\n");
-  printf("|------------------------|\n");
-  printf("| 3 | Temperature        |\n");
-  printf("|------------------------|\n");
-  printf("| 4 | Wind               |\n");
-  printf("|------------------------|\n");
-  printf("| 5 | Location           |\n");
-  printf("|------------------------|\n");
-  printf("| 6 | Precipitation      |\n");
-  printf("|------------------------|\n");
-  printf("| 7 | Astronomy          |\n");
-  printf("|------------------------|\n");
-  printf("| 8 | History            |\n");
-  printf("+------------------------+\n");
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	printf("\nWhat would you like to view for **INCLUDE LOCATION SOMEHOW***\n");
+	printf("Select the number corresponding to the function you'd like to view.\n");
+	printf("For example, 1 would display the Weather Condition, 2 would display Humidity, etc.\n");
+	printf("\n+------------------------+\n");
+	printf("| 1 | Weather Condition  |\n");
+	printf("|------------------------|\n");
+	printf("| 2 | Humidity           |\n");
+	printf("|------------------------|\n");
+	printf("| 3 | Temperature        |\n");
+	printf("|------------------------|\n");
+	printf("| 4 | Wind               |\n");
+	printf("|------------------------|\n");
+	printf("| 5 | Location           |\n");
+	printf("|------------------------|\n");
+	printf("| 6 | Precipitation      |\n");
+	printf("|------------------------|\n");
+	printf("| 7 | Astronomy          |\n");
+	printf("|------------------------|\n");
+	printf("| 8 | History            |\n");
+	printf("+------------------------+\n");
 }
 
 void getWeather()
 {
-/*prefix = https://wttr.in/
+	/*prefix = https://wttr.in/
 suffix = ?format=j1
 
 if the user enters: Paris
@@ -150,60 +148,57 @@ Make the URL: https://wttr.in/Paris?format=j1
 
 Basically it would be prefix + user input + suffix */
 
+	char prefix[] = "https://wttr.in/";
+	char suffix[] = "?format=j1";
+	char userLocation[100];
 
-  char prefix[] = "https://wttr.in/";
-  char suffix[] = "?format=j1";
-  char userLocation[100];
+	printf("Enter your location:\nFOR TESTING PURPOSES use Paris ");
+	gets(userLocation);
 
-  printf("Enter your location:\nFOR TESTING PURPOSES use Paris ");
-  gets(userLocation);
+	strcat(prefix, userLocation);
+	strcat(prefix, suffix);
+	printf("URL is %s\n", prefix);
 
+	CURL *curl_handle;
+	static const char *pagefilename = "page.json";
+	FILE *pagefile;
 
-  strcat(prefix, userLocation);
-  strcat(prefix, suffix);
-  printf("URL is %s\n", prefix);
+	curl_global_init(CURL_GLOBAL_ALL);
 
+	/* init the curl session */
+	curl_handle = curl_easy_init();
 
+	/* set URL to get here */
+	curl_easy_setopt(curl_handle, CURLOPT_URL, prefix);
 
-  CURL *curl_handle;
-  static const char *pagefilename = "page.json";
-  FILE *pagefile;
- 
-  curl_global_init(CURL_GLOBAL_ALL);
- 
-  /* init the curl session */
-  curl_handle = curl_easy_init();
- 
-  /* set URL to get here */
-  curl_easy_setopt(curl_handle, CURLOPT_URL, prefix);
- 
-  /* Switch on full protocol/debug output while testing */
-  curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
- 
-  /* disable progress meter, set to 0L to enable it */
-  curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
- 
-  /* send all data to this function  */
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
- 
-  /* open the file */
-  pagefile = fopen(pagefilename, "wb");
-  if(pagefile) {
- 
-    /* write the page body to this file handle */
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
- 
-    /* get it! */
-    curl_easy_perform(curl_handle);
- 
-    /* close the header file */
-    fclose(pagefile);
-  }
- 
-  /* cleanup curl stuff */
-  curl_easy_cleanup(curl_handle);
- 
-  curl_global_cleanup();
+	/* Switch on full protocol/debug output while testing */
+	curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+
+	/* disable progress meter, set to 0L to enable it */
+	curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+
+	/* send all data to this function  */
+	curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+
+	/* open the file */
+	pagefile = fopen(pagefilename, "wb");
+	if (pagefile)
+	{
+
+		/* write the page body to this file handle */
+		curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
+
+		/* get it! */
+		curl_easy_perform(curl_handle);
+
+		/* close the header file */
+		fclose(pagefile);
+	}
+
+	/* cleanup curl stuff */
+	curl_easy_cleanup(curl_handle);
+
+	curl_global_cleanup();
 }
 
 void getHumidity()
@@ -217,7 +212,8 @@ void getHumidity()
 	// current_condition_array is an array of objects
 	arraylen = json_object_array_length(current_condition_array);
 
-	for (i = 0; i < arraylen; i++) {
+	for (i = 0; i < arraylen; i++)
+	{
 		// get the i-th object in current_condition_array
 		current_condition_array_obj = json_object_array_get_idx(current_condition_array, i);
 		// get the humidity attribute in the i-th object
@@ -238,7 +234,8 @@ void getPrecipitation()
 	// current_condition_array is an array of objects
 	arraylen = json_object_array_length(current_condition_array);
 
-	for (i = 0; i < arraylen; i++) {
+	for (i = 0; i < arraylen; i++)
+	{
 		// get the i-th object in current_condition_array
 		current_condition_array_obj = json_object_array_get_idx(current_condition_array, i);
 
@@ -249,7 +246,6 @@ void getPrecipitation()
 		printf("Precipitation(in): %sin\n", json_object_get_string(precipIn));
 		printf("Precipitation(mm): %smm\n", json_object_get_string(precipMM));
 	}
-
 }
 
 void getWind()
@@ -263,7 +259,8 @@ void getWind()
 	// current_condition_array is an array of objects
 	arraylen = json_object_array_length(current_condition_array);
 
-	for (i = 0; i < arraylen; i++) {
+	for (i = 0; i < arraylen; i++)
+	{
 		// get the i-th object in current_condition_array
 		current_condition_array_obj = json_object_array_get_idx(current_condition_array, i);
 
@@ -272,16 +269,14 @@ void getWind()
 		windSpeedMiles = json_object_object_get(current_condition_array_obj, "windspeedMiles");
 		windSpeedKmph = json_object_object_get(current_condition_array_obj, "windspeedKmph");
 
-
 		// print out the wind attributes
 		printf("Wind Direction Degree: %s\n", json_object_get_string(windDirDegree));
 		printf("Wind Speed(mph): %smph\n", json_object_get_string(windSpeedMiles));
 		printf("Wind Speed(kmph): %s\nkmph", json_object_get_string(windSpeedKmph));
-
 	}
 }
 
-void getTemp()  // Add feels like
+void getTemp() // Add feels like
 {
 	int arraylen, i;
 	struct json_object *current_condition_obj, *current_condition_array, *current_condition_array_obj, *temp_F, *temp_C;
@@ -292,7 +287,8 @@ void getTemp()  // Add feels like
 	// current_condition_array is an array of objects
 	arraylen = json_object_array_length(current_condition_array);
 
-	for (i = 0; i < arraylen; i++) {
+	for (i = 0; i < arraylen; i++)
+	{
 		// get the i-th object in current_condition_array
 		current_condition_array_obj = json_object_array_get_idx(current_condition_array, i);
 
@@ -303,10 +299,9 @@ void getTemp()  // Add feels like
 		printf("Farenheit Temperature: %s F\n", json_object_get_string(temp_F));
 		printf("Celsuis Temperature: %s C\n", json_object_get_string(temp_C));
 	}
-
 }
 
-void getLocation()  
+void getLocation()
 {
 	int arraylen, i;
 	struct json_object *nearest_area_obj, *nearest_area_array, *nearest_area_array_obj, *areaName, *region;
@@ -318,7 +313,8 @@ void getLocation()
 	// nearest_area_array is an array of objects
 	arraylen = json_object_array_length(nearest_area_array);
 
-	for (i = 0; i < arraylen; i++) {
+	for (i = 0; i < arraylen; i++)
+	{
 		// get the i-th object in nearest_area_array
 		nearest_area_array_obj = json_object_array_get_idx(nearest_area_array, i);
 
@@ -330,27 +326,26 @@ void getLocation()
 		// printf("areaName=%s\n", json_object_get_string(areaName));
 		// printf("region=%s\n", json_object_get_string(region));
 	}
-	
+
 	// Gets the value of area Name
 
-		areaName_obj = json_object_array_get_idx(areaName,0);
+	areaName_obj = json_object_array_get_idx(areaName, 0);
 
-		areaNameVal = json_object_object_get(areaName_obj, "value");
+	areaNameVal = json_object_object_get(areaName_obj, "value");
 
-		// printf("area=%s\n", json_object_get_string(areaNameVal));
+	// printf("area=%s\n", json_object_get_string(areaNameVal));
 
 	// Gets the value of region
-		region_obj = json_object_array_get_idx(region,0);
+	region_obj = json_object_array_get_idx(region, 0);
 
-		regionVal = json_object_object_get(region_obj, "value");
+	regionVal = json_object_object_get(region_obj, "value");
 
-		// printf("region=%s\n", json_object_get_string(regionVal));
+	// printf("region=%s\n", json_object_get_string(regionVal));
 
-    printf("Location: %s, %s\n", json_object_get_string(areaNameVal), json_object_get_string(regionVal));
-
+	printf("Location: %s, %s\n", json_object_get_string(areaNameVal), json_object_get_string(regionVal));
 }
 
-void getAstronomy()  
+void getAstronomy()
 {
 	int arraylen, i;
 	struct json_object *weather_obj, *weather_array, *weather_array_obj, *moon_phase, *moonrise, *moonset, *sunrise, *sunset;
@@ -367,7 +362,8 @@ void getAstronomy()
 	// weather_array is an array of objects
 	// arraylen = json_object_array_length(weather_array);
 
-	for (i = 0; i < 1; i++) {
+	for (i = 0; i < 1; i++)
+	{
 		// get the i-th object in weather_array
 		weather_array_obj = json_object_array_get_idx(weather_array, i);
 
@@ -378,67 +374,66 @@ void getAstronomy()
 		// printf("astronomy=%s\n", json_object_get_string(areaName));
 		// printf("region=%s\n", json_object_get_string(region));
 	}
-	
+
 	// Gets the moon_phase
-	moon_phase_obj = json_object_array_get_idx(moon_phase,0);
+	moon_phase_obj = json_object_array_get_idx(moon_phase, 0);
 	moon_phase_Val = json_object_object_get(moon_phase_obj, "moon_phase");
 	printf("Moon Phase: %s\n", json_object_get_string(moon_phase_Val));
 
 	// Gets the moonrise
-	moonrise_obj = json_object_array_get_idx(moon_phase,0);
+	moonrise_obj = json_object_array_get_idx(moon_phase, 0);
 	moonrise_Val = json_object_object_get(moonrise_obj, "moonrise");
 	printf("Moonrise: %s\n", json_object_get_string(moonrise_Val));
 
 	// Gets the moonset
-	moonset_obj = json_object_array_get_idx(moon_phase,0);
+	moonset_obj = json_object_array_get_idx(moon_phase, 0);
 	moonset_Val = json_object_object_get(moonset_obj, "moonset");
 	printf("Moonset: %s\n", json_object_get_string(moonset_Val));
 
 	// Gets the sunrise
-	sunrise_obj = json_object_array_get_idx(moon_phase,0);
+	sunrise_obj = json_object_array_get_idx(moon_phase, 0);
 	sunrise_Val = json_object_object_get(sunrise_obj, "sunrise");
 	printf("Sunrise: %s\n", json_object_get_string(sunrise_Val));
-	
+
 	// Gets the sunset
-	sunset_obj = json_object_array_get_idx(moon_phase,0);
+	sunset_obj = json_object_array_get_idx(moon_phase, 0);
 	sunset_Val = json_object_object_get(sunset_obj, "sunset");
 	printf("Sunset: %s\n", json_object_get_string(sunset_Val));
-
 }
 
 // Prints out a sun design
 void sun()
 {
-  printf("\n     \\   / \n");
-  printf("      .-.\n");
-  printf("  -- (   ) --\n");
-  printf("      `-'\n");
-  printf("     /   \\ \n\n");
+	printf("\n     \\   / \n");
+	printf("      .-.\n");
+	printf("  -- (   ) --\n");
+	printf("      `-'\n");
+	printf("     /   \\ \n\n");
 }
 
 // Prints out a cloud design
 void cloud()
 {
-  printf("\n     .--. \n");
-  printf("  .-(    ).\n");
-  printf(" (___.__)__) \n\n");
+	printf("\n     .--. \n");
+	printf("  .-(    ).\n");
+	printf(" (___.__)__) \n\n");
 }
 
-// Prints out a rain design 
+// Prints out a rain design
 void rain()
 {
-  printf("\n     .--. \n");
-  printf("  .-(    ).\n");
-  printf(" (___.__)__) \n");
-  printf(" '  '  '  ' \n");
-  printf("  '  '  ' ' \n\n");
+	printf("\n     .--. \n");
+	printf("  .-(    ).\n");
+	printf(" (___.__)__) \n");
+	printf(" '  '  '  ' \n");
+	printf("  '  '  ' ' \n\n");
 }
 
 // Prints a partly cloudy design
 void partlyCloudy()
 {
-  printf("\n   \\ /\n");
-  printf("- /' '\\ .--. \n");
-  printf("  \\_ .-(    ).\n");
-  printf("  / (___.__)__) \n\n");
+	printf("\n   \\ /\n");
+	printf("- /' '\\ .--. \n");
+	printf("  \\_ .-(    ).\n");
+	printf("  / (___.__)__) \n\n");
 }
